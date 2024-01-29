dcdd56fa497f296e320f81c44e59d1a6e1226c03
Romte1 <ivan_ni224@hotmail.com>
2024-01-29 18:53:45 -0300

    Added a functionality to select grid size dinamically up to 100*100, tested and seems like it's working properly for now

--- javascript.js
+++ javascript.js
@@ -1,8 +1,10 @@
 const container = document.querySelector(".container");
+const customButton = document.querySelector("button");
 
 for (i=0; i<256; i++)    {
     let gridDiv = document.createElement('div');
     gridDiv.classList.add('square');
+    gridDiv.style = 'height: 60px; width:60px;';
     container.appendChild(gridDiv);
 }
 
@@ -10,6 +12,34 @@ let hoverDivs = document.querySelectorAll(".square");
 
 hoverDivs.forEach ((hoverDiv) => {
 hoverDiv.addEventListener ('mouseover', () => {
-    hoverDiv.style = 'background-color:black;';
+    hoverDiv.style = 'background-color:black;height: 60px; width:60px;';
     });
 });
+
+customButton.addEventListener('click', () => {
+    let squares = parseInt(prompt('What size do you want? (MAX 100)'));
+        if (squares<0 || squares>100 || isNaN(squares)==true){
+            let squares = parseInt(prompt('Invalid value, try again (1-100)'));
+        } else {
+            let hoverDivs = document.querySelectorAll(".square");
+            hoverDivs.forEach((hoverDiv) => {
+                hoverDiv.remove();
+            })
+
+            for (i=0; i<squares*squares; i++)    {
+                var sqrt = Math.sqrt((960*960)/(squares*squares));
+                console.log(sqrt);
+                let gridDiv = document.createElement('div');
+                gridDiv.classList.add('square');
+                gridDiv.style = `height:${sqrt}px; width:${sqrt}px;`;
+                container.appendChild(gridDiv);
+            }
+        }
+        let hoverDivs = document.querySelectorAll(".square");
+
+hoverDivs.forEach ((hoverDiv) => {
+hoverDiv.addEventListener ('mouseover', () => {
+    hoverDiv.style = `background-color:black;height: ${sqrt}px; width:${sqrt}px;`;
+    });
+});
+})
