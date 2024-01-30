4b8c457b5656238b479e3108b09a2707fc5f46cf
Romte1 <ivan_ni224@hotmail.com>
2024-01-30 16:15:04 -0300

    Added buttons to let the user choose between black and colorful squares, and a reset to clear the grid

--- javascript.js
+++ javascript.js
@@ -1,5 +1,9 @@
 const container = document.querySelector(".container");
-const customButton = document.querySelector("button");
+const customButton = document.querySelector(".custom");
+const reset = document.querySelector(".reset");
+const black = document.querySelector(".black");
+const color = document.querySelector(".color");
+var lastSqrt;
 
 for (i=0; i<256; i++)    {
     let gridDiv = document.createElement('div');
@@ -28,18 +32,59 @@ customButton.addEventListener('click', () => {
 
             for (i=0; i<squares*squares; i++)    {
                 var sqrt = Math.sqrt((960*960)/(squares*squares));
-                console.log(sqrt);
+                lastSqrt = sqrt;
                 let gridDiv = document.createElement('div');
                 gridDiv.classList.add('square');
                 gridDiv.style = `height:${sqrt}px; width:${sqrt}px;`;
                 container.appendChild(gridDiv);
             }
         }
-        let hoverDivs = document.querySelectorAll(".square");
 
-hoverDivs.forEach ((hoverDiv) => {
-hoverDiv.addEventListener ('mouseover', () => {
+    let hoverDivs = document.querySelectorAll(".square");
+    hoverDivs.forEach ((hoverDiv) => {
+    hoverDiv.addEventListener ('mouseover', () => {
     hoverDiv.style = `background-color:rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)});height: ${sqrt}px; width:${sqrt}px;`;
+
     });
 });
 })
+
+reset.addEventListener('click', () => {
+    let hoverDivs = document.querySelectorAll(".square");
+    hoverDivs.forEach((hoverDiv) => {
+        if (lastSqrt==undefined) {
+            hoverDiv.style = `background-color:white;height:60px;width:60px`;
+        } else {
+            hoverDiv.style = `background-color:white;height:${lastSqrt}px; width:${lastSqrt}px;`    
+        }    
+    })
+
+})
+
+black.addEventListener('click', () => {
+    let hoverDivs = document.querySelectorAll(".square");
+
+    hoverDivs.forEach ((hoverDiv) => {
+        hoverDiv.addEventListener ('mouseover', () => {
+            if (lastSqrt==undefined) {
+                hoverDiv.style = `background-color:black;height:60px;width:60px`;
+            } else {
+                hoverDiv.style = `background-color:black;height:${lastSqrt}px; width:${lastSqrt}px;`    
+            }
+        })
+    })
+})
+
+color.addEventListener('click', () => {
+    let hoverDivs = document.querySelectorAll(".square");
+
+    hoverDivs.forEach ((hoverDiv) => {
+        hoverDiv.addEventListener ('mouseover', () => {
+            if (lastSqrt==undefined) {
+                hoverDiv.style = `background-color:rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)});height:60px;width:60px`;
+            } else {
+                hoverDiv.style = `background-color:rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)});height:${lastSqrt}px; width:${lastSqrt}px;`    
+            }
+        })
+    })
+}) 
